b5cd854341d29ea2dea64ab585fe5f5675ec3ea7
jakub1024 <38464239+jakub1024@users.noreply.github.com>
2018-06-12 15:05:04 +0200

    Statystyka z testami

--- Statistics/TransactionStatistics/TransactionStatisticsProvider.cs
+++ Statistics/TransactionStatistics/TransactionStatisticsProvider.cs
@@ -16,14 +16,108 @@ namespace Statistics.TransactionStatistics
         {
             var result = new TransactionStatisticsResult();
 
-            // TODO
+
+            var income = data.Where(x => x.Date >= from && x.Date <= to && x.Value > 0);
+            var expenses = data.Where(x => x.Date >= from && x.Date <= to && x.Value < 0);
+            var balance = data.Where(x => x.Date >= from && x.Date <= to);
+
+
+
+            if (income.Count() == 0)
+            {
+                result.Balance.Average = 0;
+                result.Balance.Sum = 0;
+                result.Balance.Median = 0;
+            }
+            else
+            {
+                result.Balance.Average = balance.Average(x => x.Value);
+                result.Balance.Sum = balance.Sum(x => x.Value);
+                result.Balance.Median = (balance.OrderBy(x => x.Value).ElementAt(balance.Count() / 2).Value
+                                        + balance.OrderBy(x => x.Value).ElementAt((balance.Count() - 1) / 2).Value) / 2;
+                result.Balance.StdDeviation = (decimal)Math.Sqrt((double)balance.Average(x => x.Value * x.Value) - (double)result.Balance.Average * (double)result.Balance.Average);
+            }
+            if (expenses.Count() == 0)
+            {
+                result.Expenses.Average = 0;
+                result.Expenses.Sum = 0;
+                result.Expenses.Median = 0;
+            }
+            else
+            {
+                result.Expenses.Average = expenses.Average(x => x.Value);
+                result.Expenses.Sum = expenses.Sum(x => x.Value);
+                result.Expenses.Median = (expenses.OrderBy(x => x.Value).ElementAt(expenses.Count() / 2).Value
+                                        + expenses.OrderBy(x => x.Value).ElementAt((expenses.Count() - 1) / 2).Value) / 2;
+                result.Expenses.StdDeviation = (decimal)Math.Sqrt((double)expenses.Average(x => x.Value * x.Value) - (double)result.Expenses.Average * (double)result.Expenses.Average);
+            }
+
+            if (income.Count() == 0)
+            {
+                result.Income.Average = 0;
+                result.Income.Sum = 0;
+                result.Income.Median = 0;
+            }
+            else
+            {
+                result.Income.Average = income.Average(x => x.Value);
+                result.Income.Sum = income.Sum(x => x.Value);
+                result.Income.Median = (income.OrderBy(x => x.Value).ElementAt(income.Count() / 2).Value
+                                        + income.OrderBy(x => x.Value).ElementAt((income.Count() - 1) / 2).Value) / 2;
+                result.Income.StdDeviation = (decimal)Math.Sqrt((double)income.Average(x => x.Value * x.Value) - (double)result.Income.Average * (double)result.Income.Average);
+
+            }
+
+            String dateFormat = "yyyyMMdd";
+            if (timeStep == TimeStepType.Month)
+            {
+                dateFormat = "yyyyMM";
+            }
+            else if (timeStep == TimeStepType.Hour)
+            {
+                dateFormat = "yyyyMMddhh";
+            }
+            else
+            {
+                dateFormat = "yyyyMMdd";
+            }
+
+            if (timeStep == TimeStepType.Week)
+            {
+                dateFormat = "yyyyMMdd";
+                data.Where(x => x.Date >= from && x.Date <= to).GroupBy(i => i.Date.AddDays(-(int)i.Date.DayOfWeek).ToString(dateFormat)).ToList()
+                                .ForEach(y =>
+                                result.BarChartData.Add(new TransactionsBarEntry(
+                                    DateTime.ParseExact(y.First().Date.ToString(dateFormat), dateFormat, System.Globalization.CultureInfo.InvariantCulture),
+                                    y.Where(x => x.Value > 0).Sum(x => x.Value),
+                                    y.Where(x => x.Value < 0).Sum(x => x.Value))));
+            }
+            else
+            {
+                data.Where(x => x.Date >= from && x.Date <= to).GroupBy(i => i.Date.ToString(dateFormat)).ToList()
+                    .ForEach(y =>
+                    result.BarChartData.Add(new TransactionsBarEntry(
+                        DateTime.ParseExact(y.First().Date.ToString(dateFormat), dateFormat, System.Globalization.CultureInfo.InvariantCulture),
+                        y.Where(x => x.Value > 0).Sum(x => x.Value),
+                        y.Where(x => x.Value < 0).Sum(x => x.Value))));
+            }
+
+
+            result.Balance.MaxBar = (TransactionsBarEntry)result.BarChartData.Aggregate((i1, i2) => i1.TotalValue > i2.TotalValue ? i1 : i2); ;
+            result.Balance.MinBar = (TransactionsBarEntry)result.BarChartData.Aggregate((i1, i2) => i1.TotalValue < i2.TotalValue ? i1 : i2); ;
+
+            result.Expenses.MaxBar = (TransactionsBarEntry)result.BarChartData.Aggregate((i1, i2) => i1.PosValue > i2.PosValue ? i1 : i2); ;
+            result.Expenses.MinBar = (TransactionsBarEntry)result.BarChartData.Aggregate((i1, i2) => i1.PosValue < i2.PosValue ? i1 : i2); ;
+
+            result.Income.MaxBar = (TransactionsBarEntry)result.BarChartData.Aggregate((i1, i2) => i1.NegValue > i2.NegValue ? i1 : i2); ;
+            result.Income.MinBar = (TransactionsBarEntry)result.BarChartData.Aggregate((i1, i2) => i1.NegValue < i2.NegValue ? i1 : i2); ;
 
             return result;
         }
 
         public Task<TransactionStatisticsResult> CalculateAsync(IList<ITransaction> data, DateTime from, DateTime to, TimeStepType timeStep)
         {
-            return Task.Factory.StartNew<TransactionStatisticsResult>(() => 
+            return Task.Factory.StartNew<TransactionStatisticsResult>(() =>
             {
                 return Calculate(data, from, to, timeStep);
             });
--- StatisticsTests/TransactionsStatisticsProviderTests.cs
+++ StatisticsTests/TransactionsStatisticsProviderTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Interfaces;
+using NUnit.Framework;
+using Statistics;
+using Statistics.TransactionStatistics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,398 @@ namespace StatisticsTests
     [TestFixture]
     public class TransactionsStatisticsProviderTests
     {
-        // TODO
+ 
+        TransactionStatisticsProvider tsp = new TransactionStatisticsProvider();
+        List<ITransaction> data = new List<ITransaction>();
+
+        [Test]
+        public void SimpleTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 1, 1);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -100;
+            transaction.Date = new DateTime(2001, 2, 1);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2002, 6, 6);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 200;
+            transaction.Date = new DateTime(2001, 1, 3);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -300;
+            transaction.Date = new DateTime(2001, 6, 5);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 4, 1);
+            data.Add(transaction);
+
+
+            transaction = new Transaction();
+            transaction.Value = 10000;
+            transaction.Date = new DateTime(4000, 8, 8);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 10000;
+            transaction.Date = new DateTime(400, 8, 8);
+            data.Add(transaction);
+
+            var result = tsp.Calculate(data, new DateTime(1000, 1, 1), new DateTime(3000, 2, 3), TimeStepType.Month);
+
+            Assert.AreEqual(100, result.Balance.Sum);
+            Assert.AreEqual(-400, result.Expenses.Sum);
+            Assert.AreEqual(500, result.Income.Sum);
+
+            Assert.AreEqual(100 / 6.0, result.Balance.Average);
+            Assert.AreEqual(-400 / 2, result.Expenses.Average);
+            Assert.AreEqual(500 / 4, result.Income.Average);
+
+            Assert.AreEqual(100, result.Balance.Median);
+            Assert.AreEqual(-200, result.Expenses.Median);
+            Assert.AreEqual(100, result.Income.Median);
+
+            Assert.AreEqual(300, result.BarChartData.ElementAt(0).TotalValue);
+
+        }
+
+        [Test]
+        public void StartEndDateTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 1, 1);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -50;
+            transaction.Date = new DateTime(2001, 2, 3);
+            data.Add(transaction);
+
+
+            var result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2001, 2, 3), TimeStepType.Month);
+
+            Assert.AreEqual(50, result.Balance.Sum);
+
+        }
+
+
+        [Test]
+        public void BetweenDateTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 1, 1);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -100;
+            transaction.Date = new DateTime(2001, 2, 1);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2002, 6, 6);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 200;
+            transaction.Date = new DateTime(2001, 1, 3);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -300;
+            transaction.Date = new DateTime(2001, 6, 5);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 4, 1);
+            data.Add(transaction);
+
+
+            transaction = new Transaction();
+            transaction.Value = 10000;
+            transaction.Date = new DateTime(4000, 8, 8);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 10000;
+            transaction.Date = new DateTime(400, 8, 8);
+            data.Add(transaction);
+
+
+            var result = tsp.Calculate(data, new DateTime(2001, 1, 2), new DateTime(2001, 4, 20), TimeStepType.Month);
+
+            Assert.AreEqual(200, result.Balance.Sum);
+            Assert.AreEqual(-100, result.Expenses.Sum);
+            Assert.AreEqual(300, result.Income.Sum);
+        }
+
+        [Test]
+        public void MedianOddEvenTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 1, 1);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -50;
+            transaction.Date = new DateTime(2001, 1, 2);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 200;
+            transaction.Date = new DateTime(2001, 1, 3);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -150;
+            transaction.Date = new DateTime(2001, 2, 3);
+            data.Add(transaction);
+
+            var result = tsp.Calculate(data, new DateTime(2000, 1, 1), new DateTime(2002, 4, 2), TimeStepType.Month);
+
+            Assert.AreEqual(25, result.Balance.Median);
+            Assert.AreEqual(-100, result.Expenses.Median);
+            Assert.AreEqual(150, result.Income.Median);
+
+            result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2001, 1, 2), TimeStepType.Month);
+            Assert.AreEqual(25, result.Balance.Median);
+            Assert.AreEqual(-50, result.Expenses.Median);
+            Assert.AreEqual(100, result.Income.Median);
+
+            result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2001, 1, 3), TimeStepType.Month);
+            Assert.AreEqual(100, result.Balance.Median);
+        }
+
+        [Test]
+        public void OneTransactionTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 1, 1);
+            data.Add(transaction);
+
+            var result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2001, 1, 1), TimeStepType.Month);
+
+            Assert.AreEqual(100, result.Balance.Sum);
+            Assert.AreEqual(0, result.Expenses.Sum);
+            Assert.AreEqual(100, result.Income.Sum);
+
+            Assert.AreEqual(100, result.Balance.Average);
+            Assert.AreEqual(0, result.Expenses.Average);
+            Assert.AreEqual(100, result.Income.Average);
+
+            Assert.AreEqual(100, result.Balance.Median);
+            Assert.AreEqual(0, result.Expenses.Median);
+            Assert.AreEqual(100, result.Income.Median);
+
+            Assert.AreEqual(100, result.Balance.MaxBar.TotalValue);
+            Assert.AreEqual(100, result.BarChartData.First().TotalValue);
+            Assert.AreEqual(100, result.BarChartData.ElementAt(0).TotalValue);
+            Assert.AreEqual(0, result.BarChartData.ElementAt(0).NegValue);
+            Assert.AreEqual(100, result.BarChartData.ElementAt(0).PosValue);
+        }
+
+        [Test]
+        public void MonthTimeStepTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 2, 21);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -50;
+            transaction.Date = new DateTime(2001, 2, 12);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 50;
+            transaction.Date = new DateTime(2002, 8, 22);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -100;
+            transaction.Date = new DateTime(2002, 8, 11);
+            data.Add(transaction);
+
+            var result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2003, 4, 20), TimeStepType.Month);
+
+            Assert.AreEqual(50, result.Balance.MaxBar.TotalValue);
+            Assert.AreEqual(50, result.BarChartData.First().TotalValue);
+            Assert.AreEqual(-50, result.BarChartData.ElementAt(1).TotalValue);
+            Assert.AreEqual(-100, result.BarChartData.ElementAt(1).NegValue);
+            Assert.AreEqual(50, result.BarChartData.ElementAt(1).PosValue);
+
+        }
+
+        [Test]
+        public void DayTimeStepTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 2, 21, 9, 2, 2);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -50;
+            transaction.Date = new DateTime(2001, 2, 21, 7, 2, 2);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 50;
+            transaction.Date = new DateTime(2001, 2, 25, 5, 7, 2);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -100;
+            transaction.Date = new DateTime(2001, 2, 25, 8, 2, 2);
+            data.Add(transaction);
+
+
+
+            var result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2003, 4, 20), TimeStepType.Day);
+
+            Assert.AreEqual(50, result.Balance.MaxBar.TotalValue);
+            Assert.AreEqual(50, result.BarChartData.First().TotalValue);
+            Assert.AreEqual(-50, result.BarChartData.ElementAt(1).TotalValue);
+            Assert.AreEqual(-100, result.BarChartData.ElementAt(1).NegValue);
+            Assert.AreEqual(50, result.BarChartData.ElementAt(1).PosValue);
+        }
+
+        [Test]
+        public void HourTimeStepTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2001, 2, 21, 9, 4, 2);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -50;
+            transaction.Date = new DateTime(2001, 2, 21, 9, 8, 9);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 50;
+            transaction.Date = new DateTime(2001, 2, 25, 5, 7, 2);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -100;
+            transaction.Date = new DateTime(2001, 2, 25, 5, 2, 3);
+            data.Add(transaction);
+
+            var result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2003, 4, 20), TimeStepType.Hour);
+
+            Assert.AreEqual(50, result.Balance.MaxBar.TotalValue);
+            Assert.AreEqual(50, result.BarChartData.First().TotalValue);
+            Assert.AreEqual(-50, result.BarChartData.ElementAt(1).TotalValue);
+            Assert.AreEqual(-100, result.BarChartData.ElementAt(1).NegValue);
+            Assert.AreEqual(50, result.BarChartData.ElementAt(1).PosValue);
+
+
+        }
+        [Test]
+        public void WeekTimeStepTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2018, 6, 3);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -50;
+            transaction.Date = new DateTime(2018, 6, 9);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 50;
+            transaction.Date = new DateTime(2018, 6, 11);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -100;
+            transaction.Date = new DateTime(2018, 6, 14);
+            data.Add(transaction);
+
+
+            var result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2020, 4, 20), TimeStepType.Week);
+            Assert.AreEqual(50, result.Balance.MaxBar.TotalValue);
+            Assert.AreEqual(50, result.BarChartData.First().TotalValue);
+            Assert.AreEqual(-50, result.BarChartData.ElementAt(1).TotalValue);
+            Assert.AreEqual(-100, result.BarChartData.ElementAt(1).NegValue);
+            Assert.AreEqual(50, result.BarChartData.ElementAt(1).PosValue);
+        }
+
+        [Test]
+        public void StdDeviationTest()
+        {
+            data = new List<ITransaction>();
+            ITransaction transaction = new Transaction();
+
+            transaction = new Transaction();
+            transaction.Value = 100;
+            transaction.Date = new DateTime(2018, 6, 3);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 200;
+            transaction.Date = new DateTime(2018, 6, 9);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = 150;
+            transaction.Date = new DateTime(2018, 6, 11);
+            data.Add(transaction);
+
+            transaction = new Transaction();
+            transaction.Value = -50;
+            transaction.Date = new DateTime(2018, 6, 14);
+            data.Add(transaction);
+
+
+            var result = tsp.Calculate(data, new DateTime(2001, 1, 1), new DateTime(2020, 4, 20), TimeStepType.Month);
+            Assert.AreEqual(93.54, (double)result.Balance.StdDeviation, 0.01);
+            Assert.AreEqual(0, (double)result.Expenses.StdDeviation, 0.01);
+
+        }
+
+
     }
 }
